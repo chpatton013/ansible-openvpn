94269141b25cb3a0d5818c6283d7bdc45c932699
Christopher Patton <chpatton013@gmail.com>
2017-06-13 08:49:57 -0700

    System dependencies for OpenVPN

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # OpenVPN - Debian 8 - Ansible Playbook
 
 Configure a hardened OpenVPN server on Debian 8.
+
+## Assumptions
+
+Your network interfaces have been configured correctly. You have a loopback
+device and an ethernet device with a static IP:
+```
+/etc/network/interfaces
+---
+
+auto {{lo_interface}}
+iface {{lo_interface}} inet loopback
+
+auto {{eth_interface}}
+allow-hotplug {{eth_interface}}
+iface {{eth_interface}} inet static
+  address 192.168.1.XXX
+  netmask 255.255.255.0
+  gateway 192.168.1.1
+```
+
+Your network interface configuration has been applied:
+```
+#!/bin/bash
+---
+
+ip link set eth0 down
+ip link set eth0 up
+```
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$(id --user)" -ne "0" ]]; then
+  echo Rerunning as root: $0 $@ >&2
+  exec sudo "$0" "$@"
+fi
+
+keyserver="keyserver.ubuntu.com"
+debian_signing_key_1="7638D0442B90D010"
+debian_signing_key_2="8B48AD6246925553"
+backports_repo="deb http://http.debian.net/debian jessie-backports main"
+backports_list="/etc/apt/sources.list.d/backports.list"
+
+# Install apt-key utilities.
+apt-get update --assume-yes
+apt-get install --assume-yes \
+  apt-transport-https \
+  ca-certificates \
+  software-properties-common
+
+# Authorize Debian signing keys.
+apt-key adv --keyserver "$keyserver" --recv-keys "$debian_signing_key_1"
+apt-key adv --keyserver "$keyserver" --recv-keys "$debian_signing_key_2"
+# Add Debian backports repo.
+echo "$backports_repo" > "$backports_list"
+
+# Install updated ansible.
+apt-get update --assume-yes
+apt-get --target-release jessie-backports install --assume-yes "ansible"
